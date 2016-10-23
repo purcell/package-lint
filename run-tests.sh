c6d79da384c1e5c1d784c607f434258341bbba42
Fanael Linithien <fanael4@gmail.com>
2016-10-23 11:51:14 +0200

    Fix the testsuite cl-lib related errors on Emacs <= 24.2
    Since Emacs 24.2 predates cl-lib, it needs to be downloaded from GNU ELPA,
    and package.el needs to be initialized before we do anything.

--- run-tests.sh
+++ run-tests.sh
@@ -11,21 +11,24 @@ INIT_PACKAGE_EL="(progn
 # package-lint and cl-lib.
 "$EMACS" -Q -batch \
          --eval "$INIT_PACKAGE_EL" \
-         --eval '(package-refresh-contents)'
+         --eval '(package-refresh-contents)' \
+         --eval "(package-install 'cl-lib)"
 # Byte compile, failing on byte compiler warnings or errors
 "$EMACS" -Q -batch \
+         --eval "$INIT_PACKAGE_EL" \
          -l package-lint.el \
          --eval '(setq byte-compile-error-on-warn t)' \
          -f batch-byte-compile \
          package-lint.el package-lint-test.el
 # Lint ourselves
 "$EMACS" -Q -batch \
+         --eval "$INIT_PACKAGE_EL" \
          -l package-lint.el \
          -f package-lint-batch-and-exit \
          package-lint.el package-lint-test.el
 # Finally, run the testsuite
 "$EMACS" -Q -batch \
+         --eval "$INIT_PACKAGE_EL" \
          -l package-lint.el \
          -l package-lint-test.el \
-         --eval "$INIT_PACKAGE_EL" \
          -f ert-run-tests-batch-and-exit
