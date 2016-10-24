7f20ad401c00d4aeecaec27907fa047ef1b3cad5
Fanael Linithien <fanael4@gmail.com>
2016-10-24 13:04:25 +0200

    Revert "Add test for warning when using new libraries"
    This reverts commit dffe4ff27c4ee069de125c3c0c30a0f4e75fd773.
    
    be33b4c99 already has better tests.

--- package-lint-test.el
+++ package-lint-test.el
@@ -174,13 +174,6 @@ Alternatively, depend on (emacs \"24.3\") or greater, in which cl-lib is bundled
     (package-lint-test--run
      "(when-let ((foo (bar))) (message \"ok\"))"))))
 
-(ert-deftest package-lint-test-warn-new-libraries ()
-  (should
-   (equal
-    '((3 17 warning "You should depend on (emacs \"24.4\") if you need `nadvice'."))
-    (package-lint-test--run
-     "(require 'nadvice)"))))
-
 (ert-deftest package-lint-test-accept-new-functions-with-dep ()
   (should
    (equal
