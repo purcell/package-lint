ee39c51e5d46b36d54e1f7c45baaf2cb08f7d4f8
Steve Purcell <steve@sanityinc.com>
2016-11-05 19:45:40 +1300

    Don't promote byte compilation warnings when ignoring lint issues
    This reverts commit f2f6adb4949ead143f032755787c53b1dce4cdfc, and
    replaces it with handling in run-tests.sh.

--- run-tests.sh
+++ run-tests.sh
@@ -13,11 +13,18 @@ INIT_PACKAGE_EL="(progn
          --eval "$INIT_PACKAGE_EL" \
          --eval '(package-refresh-contents)' \
          --eval "(unless (package-installed-p 'cl-lib) (package-install 'cl-lib))"
-# Byte compile, failing on byte compiler warnings or errors
+
+# Byte compile, failing on byte compiler errors, or on warnings unless ignored
+if [ -n "${EMACS_LINT_IGNORE+x}" ]; then
+    ERROR_ON_WARN=nil
+else
+    ERROR_ON_WARN=t
+fi
+
 "$EMACS" -Q -batch \
          --eval "$INIT_PACKAGE_EL" \
          -l package-lint.el \
-         --eval '(setq byte-compile-error-on-warn t)' \
+         --eval "(setq byte-compile-error-on-warn ${ERROR_ON_WARN})" \
          -f batch-byte-compile \
          package-lint.el package-lint-test.el
 # Lint ourselves
