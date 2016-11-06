46f12c815bc791b2889292d4b32eccad9e313d1d
Fanael Linithien <fanael4@gmail.com>
2016-11-06 16:22:08 +0100

    Add a dummy version number to the test suite
    The test runner runs package-lint on the test suite, so make it lint
    cleanly.

--- package-lint-test.el
+++ package-lint-test.el
@@ -4,6 +4,7 @@
 
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;;         Fanael Linithien <fanael4@gmail.com>
+;; Version: 0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
