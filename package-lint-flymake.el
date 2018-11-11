44dd7579901e296cc2b68cc7168365e86234c72f
Jonas Bernoulli <jonas@bernoul.li>
2018-11-11 11:15:42 +0100

    Whitespace and comment cosmetics

--- package-lint-flymake.el
+++ package-lint-flymake.el
@@ -1,34 +1,36 @@
 ;;; package-lint-flymake.el --- A package-lint Flymake backend  -*- lexical-binding: t; -*-
-;;
+
 ;; Copyright (C) 2018 J. Alexander Branham (alex DOT branham AT gmail DOT com)
-;;
+
 ;; This file is not part of GNU Emacs.
-;;
+
 ;; This is free software; you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free
 ;; Software Foundation; either version 3, or (at your option) any later
 ;; version.
-;;
+
 ;; This is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 ;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 ;; for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 ;; MA 02110-1301 USA.
-;;
+
 ;;; Commentary:
-;;
+
 ;; Flymake is the built-in Emacs package to support on-the-fly syntax
 ;; checking.  This file adds support for flymake to `package-lint'.
+
 ;; Enable it by calling `package-lint-setup-flymake' from a
 ;; file-visiting buffer.  To enable in all `emacs-lisp-mode' buffers:
 ;;
 ;; (add-hook 'emacs-lisp-mode-hook #'package-lint-setup-flymake)
-;;
+
 ;;; Code:
+
 (eval-when-compile
   (require 'cl-lib))
 (require 'flymake)
