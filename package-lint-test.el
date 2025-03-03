93f8956738d140eb3e8200d16fb22cc3aecf6cbc
Steve Purcell <steve@sanityinc.com>
2025-03-03 11:11:27 +0000

    Suppress mode hooks when running tests
    No need to initialise flymake etc. when running in a user's regular
    Emacs instance.

--- package-lint-test.el
+++ package-lint-test.el
@@ -54,7 +54,8 @@ headers and provide form."
   (unless featurename
     (setq featurename "test"))
   (with-temp-buffer
-    (emacs-lisp-mode)
+    (let ((delay-mode-hooks t))
+      (emacs-lisp-mode))
     (insert (or header (format ";;; %s.el --- A test\n" featurename)))
     (insert (or version ";; Package-Version: 0\n"))
     (insert (or url ";; URL: https://package-lint.test/p?q#f\n"))
